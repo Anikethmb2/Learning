ac03b8378d67ca8f4b7a34896e8c66b8dac18fce
Aniketh M B <anikethgowda022@gmail.com>
2024-12-01 00:54:01 +0530

    created object for classExample

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Learning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Car obj = new Car();
+           obj.display();
         }
     }
 }
