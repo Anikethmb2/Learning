2a90140b114984bc11ac595b085875ab24c58e38
Aniketh M B <anikethgowda022@gmail.com>
2024-12-02 21:09:58 +0530

    invoke constructor object

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@ namespace Learning
             //Creating object to ClassExample
            Car obj = new Car();
            obj.display();
+
+           // Creating construcot
+           ConstructorExample constructorExample1 = new ConstructorExample();
+           ConstructorExample constructorExample2 = new ConstructorExample(5,"Ram");
+           Console.WriteLine("this is parameterized constructor id " + constructorExample2.id +" and name "+ constructorExample2.name);
+
         }
+        
     }
 }
