3ce40bb03b56e2157d39944a4ac4f424e45775fe
Aniketh M B <anikethgowda022@gmail.com>
2024-12-03 00:33:04 +0530

    cs project file

--- Program.cs
+++ Program.cs
@@ -10,12 +10,25 @@ namespace Learning
            Car obj = new Car();
            obj.display();
 
-           // Creating construcot
+           // Creating constructor
            ConstructorExample constructorExample1 = new ConstructorExample();
            ConstructorExample constructorExample2 = new ConstructorExample(5,"Ram");
            Console.WriteLine("this is parameterized constructor id " + constructorExample2.id +" and name "+ constructorExample2.name);
+            Console.WriteLine("--------------------Inhertance------------------------------");
+            // single inhertance
+            DerviedClass obj1 = new DerviedClass();
 
+            //----------------hierarchical inhertance----------------------------
+            Cars cars = new Cars();
+            Bus bus = new Bus();
+            cars.seatCapacity(5);
+            cars.noOfWheels(4);
+            cars.transmission();
+
+            bus.seatCapacity(45);
+            bus.noOfWheels(6);
+            bus.fuelCapacity();
         }
-        
+
     }
 }
